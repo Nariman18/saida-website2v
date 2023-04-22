4e4b83de8f1e12843566fcf169500a7d8e154e3a
Nariman Aliyev <aliyev-nariman@mail.ru>
2023-04-22 14:37:12 +0400

    day six build

--- components/PhilosophyBody.js
+++ components/PhilosophyBody.js
@@ -12,7 +12,7 @@ function PhilosophyBody() {
     <div className='relative 2xl:flex-none xl:flex-none 2xl:items-start xl:items-start flex flex-col items-center 2xl:mt-0 xl:mt-0 mt-10'>
     <em className='absolute top-[-380px] w-[450px] left-[90px] text-[29px] font-extrabold 2xl:inline-flex xl:inline-flex hidden'>«There are a thousand ways to kneel and kiss the ground. There are a thousand ways to go home again»</em>
     <ul className='absolute list-disc left-[140px] top-[-180px] text-3xl text-[#848484] font-bold opacity-90 2xl:inline-flex xl:inline-flex hidden'> <li><em>Rumi</em></li> </ul>
-    <img src='/SA.png' alt='Banner' className='2xl:absolute xl:absolute relative 2xl:w-[853px] xl:w-[853px] lg:w-[760px] md:w-[653px] sm:w-[653px] w-[310px] 2xl:h-[657px] xl:h-[657px] lg:h-[657px] md:h-[550px] h-[300px] 2xl:left-[666px] xl:left-[666px] 2xl:top-[-400px] xl:top-[-400px] object-cover'/>  
+    <img src='/Rectangle 6.png' alt='Banner' className='2xl:absolute xl:absolute relative 2xl:w-[853px] xl:w-[853px] lg:w-[760px] md:w-[653px] sm:w-[653px] w-[310px] 2xl:h-[657px] xl:h-[657px] lg:h-[657px] md:h-[550px] h-[300px] 2xl:left-[666px] xl:left-[666px] 2xl:top-[-400px] xl:top-[-400px] object-contain'/>  
     
             <div className='2xl:absolute xl:absolute relative 2xl:top-[350px] xl:top-[350px] 2xl:left-[666px] xl:left-[666px] 2xl:mt-0 xl:mt-0 mt-20'>
             <em className='flex  font-Catamaran uppercase font-[600] 2xl:text-[23px] xl:text-[23px] lg:text-[20px] text-[23px]'>History of Myself</em>
