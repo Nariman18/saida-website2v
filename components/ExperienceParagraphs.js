c43888a14ed319151514c60d22d5ed7b0553a106
Nariman Aliyev <aliyev-nariman@mail.ru>
2023-05-03 17:50:53 +0400

    day six build

--- components/ExperienceParagraphs.js
+++ components/ExperienceParagraphs.js
@@ -4,24 +4,87 @@ function ExperienceParagraphs() {
   return (
     <div className='grid 2xl:grid-cols-3 xl:grid-cols-3 lg:grid-cols-3'>
         <div className='2xl:mt-[90px] xl:mt-[90px] lg:mt-[90px] mt-[120px] 2xl:ml-[130px] xl:ml-[130px] lg:ml-[150px]'>
-            <em className='flex uppercase font-[600] 2xl:text-[23px] xl:text-[23px] lg:text-[20px] text-[23px]'>History of Myself</em>
-            <p className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]'>I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning</p>
-            <p className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]'>I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning</p>
-            <h4 className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200] underline underline-offset-[3px] decoration-1 decoration-gray-300'>Meaning</h4>
+            <p className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]'>Saida Abdullayeva is a Baku and New York-based
+Executive and Life Coach and a Medical Doctor.
+She graduated from Azerbaijan Medical University with an
+honorary diploma, specializing in Ophthalmology.</p>
+            <p className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]'>Dr. Abdullayeva continued studying and working in New
+York, USA. Studying medicine and working in the US
+made her realize that the existing approach towards health
+and wellbeing is not comprehensive enough. This
+prompted her to take a quest and find a way to expand her
+knowledge and expertise beyond classical medical
+training.</p>
+            <h4 className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]  decoration-1 decoration-gray-300'>Subsequently, Dr. Abdullayeva studied and graduated
+from an ICF (International Coaching Federation)
+accredited coaching certification program in New York
+with excellent results as an Executive and Life Coach.</h4>
         </div>
 
         <div className=' 2xl:mt-[630px] xl:mt-[630px] mt-[120px] lg:mt-[630px] 2xl:ml-[150px] xl:ml-[150px] lg:ml-[60px]'>
-            <em className='font-Catamaran uppercase font-[600] 2xl:text-[23px] xl:text-[23px] lg:text-[20px] text-[23px]'>History of Myself</em>
-            <p className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]'>I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning</p>
-            <p className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]'>I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning</p>
-            <h4 className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200] underline underline-offset-[3px] decoration-1 decoration-gray-300'>Meaning</h4>
+            
+            <p className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]'>Dr. Abdullayeva is a unique coach who incorporates her
+knowledge of the human body and human psychology into
+her coaching practice. Being a practicing doctor, she has
+been engaging both with patients and coaching clients,
+bridging the two central aspects of human wellbeing-
+physical and emotional health.
+Dr. Abdullayeva specializes in transformational,
+organizational, and cross-cultural coaching. Areas of her
+expertise include emotional intelligence, leadership and
+public speaking, conflict resolution, and stress
+management.
+
+Dr. Abdullayeva utilizes her knowledge of the
+neuroplasticity of the brain and helps her clients to rewire
+their brains, and essentially redesign their entire life.</p>
+            <p className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]'>Emotional Intelligence plays one of the core roles in her
+practice. She focuses on self-awareness and facilitates the
+improvement of communication skills.
+Dr. Abdullayeva’s leadership and public speaking skills
+expertise stems from her experience with one of the oldest
+public speaking clubs in the US- “Toastmasters”.
+Established in 1924 the club currently is present in 144
+countries worldwide. She was an active member for a
+while and subsequently was elected as a Vice President of
+“New York Toastmasters”. She led the membership
+program, engaging with members, and helping them to
+improve their communication, public speaking, and
+leadership skills. In this capacity, she received vocal
+training, which she also incorporates into her coaching.
+Additionally, by studying the history of humanity via the
+Metropolitan Museum of Art, Dr. Abdullayeva has gained
+a comprehensive view of the anthropological aspects of
+history. Studying the rise and collapse of different
+civilizations, she acquired a more expanded understanding
+of the motivation behind human behavior. This also
+allowed her to study different leadership tactics that were
+implemented as humanity was progressing.</p>
+            <h4 className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200] decoration-1 decoration-gray-300'>Dr. Abdullayeva is an amateur athlete, who has been
+practicing different kinds of sports. It offered her an
+opportunity to study the human capacity to endure and
+manage physical and emotional stress. This experience
+informs Dr. Abdullayeva‘s focus on body awareness and
+body and mind connection while coaching. She offers her
+clients guidance to manage stress, tailored to the needs of
+a fast-paced modern world.
+Living and working between two continents, Dr.
+Abdullayeva has mastered cultural fluidity. Her culturally
+sensitive coaching prepares her clients for the challenges
+of the global business environment.</h4>
         </div>
 
         <div className='2xl:mt-[1135px] xl:mt-[1135px] lg:mt-[1135px] mt-[120px] 2xl:ml-[150px] xl:ml-[150px] lg:ml-[35px]'>
-            <em className='font-Catamaran uppercase font-[600] 2xl:text-[23px] xl:text-[23px] lg:text-[20px] text-[23px]'>History of Myself</em>
-            <p className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]'>I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning</p>
-            <p className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]'>I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning</p>
-            <h4 className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200] underline underline-offset-[3px] decoration-1 decoration-gray-300'>Meaning</h4>
+          
+            <p className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]'>Dr. Abdullayeva is currently working on an educational
+project dedicated to reshaping the healthcare culture in
+Azerbaijan. She also is leading an effort of bringing
+organizational and executive coaching to Azerbaijan on
+more expanded bases.</p>
+            <p className='mt-10 text-[#848484] text-left w-[310px] text-[16px] font-georgia font-[200]'>Dr. Abdullayeva works with private clients and companies,
+offering coaching in Azerbaijani, English, and Russian
+languages.</p>
+           
         </div>
     </div>
   )
