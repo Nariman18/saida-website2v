0006f8cdd29046d4176d5db87ddd657aa7df7da6
Nariman Aliyev <aliyev-nariman@mail.ru>
2023-05-04 14:24:12 +0400

    day six build

--- components/PhilosophyBody.js
+++ components/PhilosophyBody.js
@@ -16,23 +16,100 @@ function PhilosophyBody() {
     
             <div className='2xl:absolute xl:absolute relative 2xl:top-[350px] xl:top-[350px] 2xl:left-[666px] xl:left-[666px] 2xl:mt-0 xl:mt-0 mt-20'>
             
-            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200]'>I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning</p>
-            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200]'>I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning</p>
-            <h4 className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200] underline underline-offset-[3px] decoration-1 decoration-gray-300'>Meaning</h4>
+            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200]'>Hello, in this portion of the website, you will have an
+opportunity to
+get to know me better as a person and as a coach. My path,
+geographically speaking has taken me through different
+continents and each offered life-changing experiences.</p>
+            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200]'>Growing up in a loving and traditional Azerbaijani family I
+was fortunate enough
+to experience a personal renaissance after moving to the
+United States,
+to New York City to be more precise. If I had to describe
+New
+York in a short manner, I’d say that it is a place that lacks
+mercy
+and consequentially offers unfiltered truth, for which I am
+very grateful.</p>
+            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200] decoration-1 decoration-gray-300'>Being a Medical Doctor paved the beginning of my path in
+New
+York. Experiencing a healthcare system of post-socialistic
+and then
+capitalistic societies gave me a unique outlook on my
+profession
+and life in general. I understood that the way medicine is
+being
+practiced in the US, wasn’t the way I wanted to practice it.
+I
+disagreed with the lack of holistic approach of the system.
+I knew
+that human beings are not merely a sum of biochemical
+
+processes that occur within their bodies. I knew there is
+more
+than meets the eye.</p>
             </div>  
 
-            <div className='2xl:absolute xl:absolute relative 2xl:top-[760px] xl:top-[760px] 2xl:left-[666px] xl:left-[666px] 2xl:mt-0 xl:mt-0 mt-20'>
+            <div className='2xl:absolute xl:absolute relative 2xl:top-[1070px] xl:top-[760px] 2xl:left-[666px] xl:left-[666px] 2xl:mt-0 xl:mt-0 mt-20'>
            
-            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200]'>I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning</p>
-            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200]'>I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning</p>
-            <h4 className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200] underline underline-offset-[3px] decoration-1 decoration-gray-300'>Meaning</h4>
+            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200]'>Disagreement is often an opportunity where one can look
+for
+new pathways towards growth. I took the chance. A long
+and arduous search that braved many amazing
+professional, and educational experiences, ended with the
+discovery of
+Coaching. After becoming a Life and Executive Coach, and
+working in New York, I chose to travel to Africa. This trip
+was nothing less than revolutionary. I was transported
+from a concrete jungle representing human ambition to
+the vast and magnificent Sahara that commanded
+humility.</p>
+            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200]'>We often think that the ocean is vast, but the desert has its
+own way of vastness that makes you feel insignificant and
+yet content with that role.
+Encountering Saharan nomads that lived by unforgiving
+rules and laws of the desert, made me realize that we all in
+the modern world are consumed and owned by what we
+call progress. Moving throughout the desert with a
+minimal number of personal belongings, these people
+accept her reign and live with much more integrity and
+alignment than many of us in the modern world. This was
+a great humility lesson.</p>
+            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200] decoration-1 decoration-gray-300'>These are a few life events and experiences that affected
+and shaped me as a person and as a coach. Needless to say,
+that I bring all of it to my practice with the goal to serve
+
+people, as coaching for me is an opportunity to work with
+people’s minds
+and hearts. The physical aspect of life is undeniable, but
+we often
+forget that the main portion of it is being controlled by our
+choices.</p>
             </div> 
 
-            <div className='2xl:absolute xl:absolute relative 2xl:top-[1170px] xl:top-[1170px] 2xl:left-[666px] xl:left-[666px] 2xl:mt-0 xl:mt-0 mt-20'>
+            <div className='2xl:absolute xl:absolute relative 2xl:top-[1950px] xl:top-[1170px] 2xl:left-[666px] xl:left-[666px] 2xl:mt-0 xl:mt-0 mt-20'>
             
-            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200]'>I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning</p>
-            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200]'>I love art only if it has a meaning I love art only if it has a meaning I love art only if it has a meaning</p>
-            <h4 className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200] underline underline-offset-[3px] decoration-1 decoration-gray-300'>Meaning</h4>
+            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200]'>Today’s science proves that our physical body
+listens to commands of our mind. The way you think is in
+direct correlation with the way your body decides to feel
+sleepy, anxious, joyous or calm. I help my clients to change
+how their brain functions by implementing techniques
+based on currently available scientific knowledge.</p>
+            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200]'>My job,
+whether it be
+Executive or Life Coaching, is to help my clients to gain
+new skills, and new emotional and intellectual habits that
+will
+help them to change their life.
+I wholeheartedly believe that anyone can achieve the
+highest
+goals and change their life.</p>
+            <p className='text-[#848484] text-left w-[310px] text-[16px] font-Catamaran font-[200] decoration-1 decoration-gray-300'>It is never late and it is always
+a
+good time to start doing it.
+Picasso famously said “It takes a long time to become
+young.”
+I say it takes as long as it takes for you to start. Just start.</p>
             </div> 
         <img src='/Pixabay.png' alt='Pic' className='absolute object-cover w-[230px] h-[480px] top-[730px] 2xl:inline-flex xl:inline-flex hidden' />
         <em className='absolute top-[1200px] w-[450px] left-[90px] text-[29px] font-extrabold 2xl:inline-flex xl:inline-flex hidden'>«There are a thousand ways to kneel and kiss the ground. There are a thousand ways to go home again»</em>
