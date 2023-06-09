c4f0e76ee58b09decb82dce6629df82fc7db2083
Nariman Aliyev <aliyev-nariman@mail.ru>
2023-06-09 15:57:52 +0400

    day six build

--- components/ExperienceParagraphs.js
+++ components/ExperienceParagraphs.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
 
--- components/WhatCoachingParagraph.js
+++ components/WhatCoachingParagraph.js
@@ -1,6 +1,6 @@
 import useTranslation from 'next-translate/useTranslation'
 import { useRouter } from 'next/router';
-import React from 'react'
+import React, { useEffect } from 'react'
 
 
 function WhatCoachingParagraph() {
