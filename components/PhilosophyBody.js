11c23d72f884a5a258302df467e809037880cbc6
Nariman Aliyev <aliyev-nariman@mail.ru>
2023-06-09 19:47:43 +0400

    day six build

--- components/PhilosophyBody.js
+++ components/PhilosophyBody.js
@@ -1,9 +1,11 @@
 import useTranslation from 'next-translate/useTranslation'
+import { useRouter } from 'next/router';
 import React, { useEffect } from 'react'
 
 function PhilosophyBody() {
 
   const { t } = useTranslation('common')
+  const router = useRouter()
 
   useEffect(() => {
     router.push(router.pathname, router.asPath, { locale: 'en' });
