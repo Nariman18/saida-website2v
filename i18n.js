65da00f6d50633e2546552b6d0e5d4bd4be60808
Nariman <aliyevnariman98@gmail.com>
2025-07-08 18:41:23 +0200

    day seven build

--- i18n.js
+++ i18n.js
@@ -1,7 +1,7 @@
 module.exports = {
-  locales: ['en', 'az', 'ru'], // Array with the languages that you want to use
-  defaultLocale: 'en', // Default language of your website
+  locales: ["en", "az", "ru"], // Array with the languages that I want to use
+  defaultLocale: "en", // Default language of this website
   pages: {
-    '*': ['common'], // Namespaces that you want to import per page (we stick to one namespace for all the application in this tutorial)
+    "*": ["common"], // Namespaces that I import per page (I am stick to one namespace for all the application in this tutorial)
   },
 };
