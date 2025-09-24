da8e834c63af7b069af368dae9ac1e1a2b00739f
Nariman <aliyevnariman98@gmail.com>
2025-09-24 20:13:22 +0200

    day seven build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextTranslate = require('next-translate-plugin') 
+const nextTranslate = require("next-translate-plugin");
 
 module.exports = nextTranslate({
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 });
